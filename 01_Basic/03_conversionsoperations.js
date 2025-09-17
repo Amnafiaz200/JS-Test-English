f7ee9daf2e93bab8c1cf03860c1160c1f86e9338
Amnafiaz200 <amna30042002@gmail.com>
2025-09-17 05:50:20 +0000

    conversions

---
+++ 01_Basic/03_conversionsoperations.js
@@ -0,0 +1,81 @@
+// let score1= 33
+// console.log(typeof score1)
+
+// score2= "33"
+// console.log(typeof score2)
+
+// let score3= '123abc'
+// let valueInNumber= Number(score3) //convert to number
+
+// console.log(typeof score3)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) //NaN not a number, when we try to convert a non number string to number
+
+// score4= true
+// console.log(typeof score4)
+
+// let valueInNumber2= Number(score4) //true is 1, false is 0
+// console.log(typeof valueInNumber2)
+// console.log(valueInNumber2)
+
+// let score5= "33.44"
+// let valueInNumber3= parseFloat(score5) //convert to number
+// console.log(typeof valueInNumber3)
+// console.log(valueInNumber3)
+
+// let score6= "33.43abc"
+// let valueInNumber4= parseFloat(score6) //decimal convert to number, it will take only starting number and ignore rest
+// console.log(typeof valueInNumber4)
+// console.log(valueInNumber4)
+
+// let score7= "33abc44"
+// let valueInNumber5= parseInt(score7) //convert to number, it will take only starting number and ignore rest
+// console.log(typeof valueInNumber5)
+// console.log(valueInNumber5)     //parseInt convert to integer, parseFloat convert to decimal number
+
+// let score8= 0
+// let valueInBoolean= Boolean(score8) //0 is false, any other number is true
+// console.log(typeof valueInBoolean)
+// console.log(valueInBoolean)
+
+// let score9= "abc"
+// let valueInBoolean2= Boolean(score9) //any string is true except empty string
+// console.log(typeof valueInBoolean2)
+// console.log(valueInBoolean2)
+
+// let score10= ""
+// let valueInBoolean3= Boolean(score10) //empty string is false
+// console.log(typeof valueInBoolean3)
+// console.log(valueInBoolean3)
+
+// let score11= null
+// let valueInBoolean4= Boolean(score11) //null is false
+// console.log(typeof valueInBoolean4)
+// console.log(valueInBoolean4)
+
+// let score12= undefined
+// let valueInBoolean5= Boolean(score12) //undefined is false
+// console.log(typeof valueInBoolean5)
+// console.log(valueInBoolean5)    
+//Boolean conversion: 0, null, undefined, NaN, "" are false, rest all are true
+
+let score13= 123
+let valueInString= String(score13) //convert to string
+console.log(typeof valueInString)
+console.log(valueInString)
+
+let score14= true
+let valueInString2= String(score14) //convert to string
+console.log(typeof valueInString2)
+console.log(valueInString2)
+
+let score15= null
+let valueInString3= String(score15) //convert to string
+console.log(typeof valueInString3)
+console.log(valueInString3)
+
+
+
+
+
+
