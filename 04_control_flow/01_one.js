8b348d1726324089b927ef440a4a98dec5744e37
Amnafiaz200 <amna30042002@gmail.com>
2025-09-26 11:06:13 +0000

    switch

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,46 @@
+// control flow or logic flow
+// code should be run on conditional basis
+// if (condition true or false){scope}
+
+// = operator assignment, == is comparison operator, === check value and datatype.
+// if (2!=4){
+//     console.log('amna'); // if condition is true=> print console
+    
+// }
+let temp = 50
+// if (temp <= 49){
+//     const fewer = 'high'
+//     console.log(`the fewer is ${fewer}`);
+// }
+// else
+//     {console.log('the fewer is low')};
+
+// if (temp <= 49){
+//     const fewer = 'high'
+//     console.log(`the fewer is ${fewer}`);
+// }
+// else
+//    {console.log(`the fewer is ${fewer}`)}; // // error bz fewer variable is out of scope of {}
+// if use var(global scoped), the above can be assessed outside the if scope
+
+// implicit scope
+// if (temp >= 49) console.log('high'); 
+
+// check multiple conditions:
+let debitcard = true
+let creditcard = true
+
+if (debitcard && creditcard) console.log('u can buy'); //check both conditions
+//  || it is or symbol.
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,38 @@
+//check value by multiple conditions easily
+// switch case use in redux
+// switch (key) {
+//     case value:                //syntax
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let month = 3
+switch (month) {
+    case 1:                       //shift alt downarraow(duplicate)
+        console.log('january');
+        
+        break;
+    case 2:                       //shift alt downarraow(duplicate)
+        console.log('feb');
+        
+        break;
+    case 3:                       //shift alt downarraow(duplicate)
+        console.log('march');     // if break not apply, where case match, from there to end, execute all the code except default
+        
+        break;
+    case 4:                       //shift alt downarraow(duplicate)
+        console.log('april');
+        
+        break;
+        
+    default:
+        console.log('defaut case'); // print march
+        
+        break;
+}
+
+
+
